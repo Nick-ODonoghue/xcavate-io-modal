5f4d170295239949c8808dc972e1a3bb9d4635d7
Nick-ODonoghue <NicholasGODonoghue@gmail.com>
2023-07-06 14:11:05 +0100

    initial commit, next boilerplate code removed

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <div>
+        <h1>Hello</h1>
+      </div>
+    </main>
+  );
+}
