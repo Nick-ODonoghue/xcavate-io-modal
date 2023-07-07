9daa5ab69e2cb1e4982237be75ed044a8e7860e4
Nick-ODonoghue <NicholasGODonoghue@gmail.com>
2023-07-07 15:53:25 +0100

    left modal done, button styled

--- components/Button.jsx
+++ components/Button.jsx
@@ -3,7 +3,11 @@
 export default function Button({ onClick }) {
   return (
     <>
-      <button className=" bg-red-400 py-2 px-4 rounded-lg" type="button" onClick={onClick}>
+      <button
+        className=" font-bold border-2 border-slate-50 py-2 px-4 rounded-lg transition-all duration-300 hover:border-slate-950 hover:text-slate-50"
+        type="button"
+        onClick={onClick}
+      >
         Click Me
       </button>
     </>
