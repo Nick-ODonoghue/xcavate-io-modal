fe854dec423bff43d313e97091d33136e4ae6c47
Nick-ODonoghue <NicholasGODonoghue@gmail.com>
2023-07-07 18:57:57 +0100

    installed Graphik fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,12 @@ module.exports = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        graphikRegular: ["GraphikRegular", "sans-serif"],
+        graphikMedium: ["GraphikMedium", "sans-serif"],
+        graphikSemibold: ["GraphikSemibold", "sans-serif"],
+        graphikBold: ["GraphikBold", "sans-serif"],
+      },
     },
   },
   plugins: [],
